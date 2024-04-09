eca31f39f585dbda704f1e20beadec6f961a6b78
Rey <reydavidquiroz@gmail.com>
2024-04-08 20:56:23 -0500

    vite2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://reymq28.github.io/find-your-id",
+  base: "https://ReyMQ28.github.io/find-your-id",
 })
